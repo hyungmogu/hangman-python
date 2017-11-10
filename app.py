1d90b7f5ec5793da536e2466b7ef2c15ed6f3495
Hyungmo Gu <guhyungm7@gmail.com>
2017-11-10 15:42:36 -0800

    feat: Refine top-level pseudocode

--- app.py
+++ app.py
@@ -22,6 +22,7 @@
 	- var hidden_phrase to display letters and/or hypens
 	- var guess to store guess from a user
 	- var response to store user's decision to play game again or not
+	- var num_of_lives == 6, to use it for displaying gallow
 
 
 
@@ -42,10 +43,80 @@
 				play_game = False 
 
 
+	step 2
+	------
+
+		initialize game
+		---------------
+			- in the beginning of the pseudocode, user is prompted to enter secret phrase
+			- after entering the secret phrase, hidden phrase is generated. It is secret phrase where all letters are replaced with hypens.
+			- before displaying hidden phrase and gallow, the current screen is cleared
+			- after clearing screen, gallow and hidden phrase are displayed
+			- at the end of the pseudoce, the game is setup for user to play
 
 
+			based on the requirements, the pseudocode can be refined to the following:
+
+			num_of_lives = 6
+			secret_phrase = create_secret_phrase()
+			hidden_phrase = generate_hidden_phrase(secret_phrase)
+			clear_screen()
+			show_gallow(num_of_lives)
+			show_hidden_phrase(hidden_phrase)
+			
+
+		play game
+		---------
+			- in the beginning of the pseudocode, user is prompted to enter a guess
+			- after entering a guess, guess is compared with secret phrase
+			- if user makes a correct guess, all matching letters are shown
+			- if user makes an incorrect guess, a body part is added to the gallow, and num_of_lives is decremented 
+			- the game repeats until num_of_lives == 0 or hidden_phrase == secret_phrase
+
+			based on the requirement, the pseudocode can be refined to the following
+
+			def play_game(num_of_lives, secret_phrase, hidden_phrase):
+
+				while(game_is_not_over(num_of_lives, hidden_phrase, secret_phrase)):
+
+					guess = make_a_guess()
+					result = compare_guess(guess, secret_phrase, hidden_phrase, num_of_lives)
+					show_gallow(num_of_lives)
+					show_hidden_phrase(hidden_phrase)
 
 
+		display result
+		--------------
+			- in the beginning of the pseudocode, the recentmost gallow is shown
+			- after showing the gallow, it displays if the user has won or lost
+			- after printing the outcome, it displays the secret phrase
+
+
+			based on the requirement, the pseudocode can be refined to the following:
+
+			- show gallow
+			- print the outcome of the game (won/lost)
+			- display secret phrase
+
+			the above pseudocde can be turned into the following python syntax
+
+			def display_result(num_of_lives, hidden_phrase, secret_phrase):
+				show_gallow(num_of_lives)
+				print_outcome_of_game(hidden_phrase,secret_phrase,num_of_lives)
+				print_secret_phrase(secret_phrase)
+
+
+		play again
+		----------
+			- in the beginning of the pseudocode, user is prompted to play game again or not
+			- at the end of the pseudocode, it returns true if user wants to play again and false if user doesn't want to play again
+
+
+			based on the requirements, the pseudocode can be refined to the following:
+
+			- prompt user to play game again or not (y|n)
+			- if 'y', return true
+			- if 'n', return false
 
 '''
 
